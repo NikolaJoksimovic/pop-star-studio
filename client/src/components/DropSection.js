49b47d5d1321356c92fe383ea847a8329aec321a
unknown <nikolajoksimovic95@gmail.com>
2022-11-14 21:55:22 +0100

    add custom height for drop sections based on num of items in them

--- client/src/components/DropSection.js
+++ client/src/components/DropSection.js
@@ -2,8 +2,17 @@ import React from "react";
 
 const DropSection = ({ name, children }) => {
   const handleDropClick = (e) => {
-    console.log(e.target.parentElement.childNodes[1]);
-    let dropSectionEl = e.target.parentElement.childNodes[1];
+    // console.log(e.target.parentElement.childNodes[1].childElementCount);
+    const dropSectionEl = e.target.parentElement.childNodes[1];
+    const chldElCount = e.target.parentElement.childNodes[1].childElementCount;
+
+    if (dropSectionEl.classList.contains("drop-section-show")) {
+      dropSectionEl.style.height = `0px`;
+    } else {
+      dropSectionEl.style.height = `${
+        dropSectionEl.parentElement.offsetHeight * chldElCount
+      }px`;
+    }
     dropSectionEl.classList.toggle("drop-section-show");
   };
   return (
